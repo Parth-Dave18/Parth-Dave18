6a25d3a41cc35cd3ddbebac0f66dc4fee7acf5eb
Parth-Dave18 <parthdave2602@gmail.com>
2023-05-10 17:42:57 +0530

    Bottom navigation bar

--- app/src/main/java/com/example/r_cat/MainActivity.java
+++ app/src/main/java/com/example/r_cat/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.r_cat;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.r_cat.ui.home.HomeFragment;
+import com.example.r_cat.ui.learning.LearnFragment;
+import com.example.r_cat.ui.slideshow.SlideshowFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private BottomNavigationView bnView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +58,38 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        bnView = findViewById(R.id.bottom_nav);
+        bnView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.bottom_nav_home){
+                    changeFragmentBn(new HomeFragment(),true);
+                }
+                else if(id == R.id.bottom_nav_learn){
+                    changeFragmentBn(new LearnFragment(),false);
+                }
+                else{
+                    changeFragmentBn(new SlideshowFragment(),false);
+                }
+
+            }
+        });
+        bnView.setSelectedItemId(R.id.bottom_nav_learn);
+
+    }
+
+    private void changeFragmentBn(Fragment fragment,boolean flag) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if(flag) {
+            ft.add(R.id.nav_host_fragment_content_main, fragment);
+        }
+        else {
+            ft.replace(R.id.nav_host_fragment_content_main,fragment);
+        }
+        ft.commit();
     }
 
     @Override
